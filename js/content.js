10589364078d06841780ace75a22d18bf10d5f5c
Balthazar GRONON <bgronon@gmail.com>
2013-12-30 12:54:57 +0100

    fix

--- js/content.js
+++ js/content.js
@@ -5,34 +5,33 @@ $(".peering ul:first a").each(function(){
 
 	$(this).text($(this).text().replace("noter le groupe " + $(".peering h3:first a:last").text(), "noter"));
 	var link = $(this);
-	var text = link.text().replace("devez noter ", "")
-	$.ajax({
-		url: "https://dashboard.42.fr/crawler/pull/" + text + "/",
-		dataType: "json",
-		success: function(result){
-			if (link.next().id != "review" + text)
+	var text = link.text().replace("devez noter ", "");
+	if (text != "avez donné la note")
+	{
+		$.ajax({
+			url: "https://dashboard.42.fr/crawler/pull/" + text + "/",
+			dataType: "json",
+			success: function(result){
 				link.after(' (<span style="color: #01824A;font-weight:bold;">' + result.last_host.replace(".42.fr", "") + "</span>)");
-		},
-		error: function(result){
-			if (link.next().id != "review" + text)
+			},
+			error: function(result){
 				link.after(' (<span style="color: #F5634A;font-weight:bold;">Logout</span>)');
-		}
-	});
+			}
+		});
+	}
 });
 
-$(".peering a[title]").each(function(){
+$(".peering ul:last a[title]").each(function(){
 
 	var link = $(this);
 	$.ajax({
 		url: "https://dashboard.42.fr/crawler/pull/" + link.text() + "/",
 		dataType: "json",
 		success: function(result){
-			if (link.next().id != "correc" + link.text())
-				link.after(' (<span style="color: #01824A;font-weight:bold;">' + result.last_host.replace(".42.fr", "") + "</span>)");
+			link.after(' (<span style="color: #01824A;font-weight:bold;">' + result.last_host.replace(".42.fr", "") + "</span>)");
 		},
 		error: function(result){
-			if (link.next().id != "correc" + link.text())
-				link.after(' (<span style="color: #F5634A;font-weight:bold;">Logout</span>)');
+			link.after(' (<span style="color: #F5634A;font-weight:bold;">Logout</span>)');
 		}
 	});
 });
